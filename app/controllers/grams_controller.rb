6f7d108149374493d1f3ec6f97aeec06c3f47355
Austin Atwood <austinatwoodjs30@gmail.com>
2020-02-08 00:26:28 +0000

    update

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,10 +5,27 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+     return render_not_found if @gram.blank?
+  end
+
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-        render plain: 'Not Found :(', status: :not_found
+    return render_not_found if @gram.blank?
+    
+  end
+
+  def update 
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
     end
   end
 
@@ -32,4 +49,7 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
 
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
+  end
 end
